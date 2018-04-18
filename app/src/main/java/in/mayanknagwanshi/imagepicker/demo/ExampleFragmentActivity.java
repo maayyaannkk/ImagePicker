4156df02ec7b877f69c4727fda195e0dd320a6ff
Mayank <maayyaannkk@gmail.com>
2018-04-18 14:52:04 +0530

    feat: fragment example and other changes

---
+++ app/src/main/java/in/mayanknagwanshi/imagepicker/demo/ExampleFragmentActivity.java
@@ -0,0 +1,13 @@
+package in.mayanknagwanshi.imagepicker.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExampleFragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example_fragment);
+    }
+}
